0056586e966d745abefd074e92d6d391127eb69c
Jason Knoll <jason.p.knoll@gmail.com>
2021-06-13 02:30:08 -0700

    Add basic structure to dencrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,15 +2,39 @@ require './lib/key'
 require './lib/date'
 
 class Enigma
-  attr_reader :message, :date, :key
+  attr_reader :message, :date, :key, :offsets
 
   def initialize
     @key = Key.new
     @date = Date.new
     @message = message
+    @offsets = offsets
+    @offsets_count = 0
+  end
+
+  def final_offsets(key, date)
+    @offsets = Offsets(key, date)
+    offset_a =
   end
 
   def encrypt(message, key, date)
+    final_offset(key, date)
+    message_to_array = message.downcase.split('')
+    message_to_array.map do |character|
+      if offsets_count = 0
+        character_set.find_index(character)
+        offsets_count += 1
+      elsif offsets_count = 1
+        character
+        offsets_count += 1
+      elsif offsets_count = 2
+        character
+        offsets_count += 1
+      else offsets_count = 3
+        character
+        @offsets_count = 0
+      end
+    end
   end
 
   def decrypt(ciphertext, key, date)
